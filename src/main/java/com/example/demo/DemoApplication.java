1d2fc4420b611a1b696471ccfa19d9a5dc9b05d9
qudwns6122 <qudwns6122@naver.com>
2020-10-19 10:28:09 +0900

    "minproject"

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		HelloSpring hs = new HelloSpring();
 		log.info(hs.print());
-asdfasdf
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
